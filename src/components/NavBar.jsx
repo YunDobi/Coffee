60495120f5e889f9e2ce45b985595c4b786d886e
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-07-18 14:54:15 -0700

    readme edit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const NavBar = () => {
   return (
     <div className="navContainer">
-      <h1 style={{margin:"0", marginLeft:"30px"}}>Brew Holic</h1>
+      <h1 style={{margin:"0", marginLeft:"30px"}}><a href="#" style={{color: "black"}}>Brew Holic</a></h1>
       <div className="NavMenu">
         {/* <span><a href="#intro">Detail</a></span>
         <span><a href="#menu">Menu</a></span>
